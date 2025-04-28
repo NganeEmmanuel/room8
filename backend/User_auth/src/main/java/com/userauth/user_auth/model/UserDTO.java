30312fa5838b2e9bb95d5d409e78f171a8ce7b3f
Ngane Emmanuel <97377179+NganeEmmanuel@users.noreply.github.com>
2025-04-28 07:41:23 -0700

    Revert "uml diagrams"

---
+++ backend/User_auth/src/main/java/com/userauth/user_auth/model/UserDTO.java
@@ -0,0 +1,25 @@
+package com.userauth.user_auth.model;
+
+import lombok.*;
+
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+
+    public UserDTO(String firstName,String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
