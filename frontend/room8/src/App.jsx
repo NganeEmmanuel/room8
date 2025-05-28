8bc3dce8e04afe75a3bcf9bce504b8b19f4d4c17
T-Bildad <tekoh.fon@ictuniversity.edu.cm>
2025-05-28 19:29:50 +0100

    Ourservices page

--- frontend/room8/src/App.jsx
+++ frontend/room8/src/App.jsx
@@ -12,6 +12,7 @@ import ManageListings from './pages/admin/ManageListings/ManageListingsPage';
 import ManageBids from './pages/admin/ManageBids/ManageBidsPage';
 import ProfilePage from './pages/admin/ProfilePage/ProfilePage';
 import ListingsSearchResultsPage from './pages/ListingsSearchResultsPage';
+import OurServicesPage from './pages/OurServicesPage/OurServicesPage';
 
 
 import PublicLayout from './layouts/PublicLayout/PublicLayout';
@@ -30,6 +31,7 @@ function App() {
           <Route path="/listings" element={<ListingsPage />} />
           <Route path="/listingDetails" element={<ListingDetailsPage />} />
           <Route path="/listings/search/:term" element={<ListingsSearchResultsPage />} />
+          <Route path="/ourservices" element={<OurServicesPage />} />
 
         </Route>
 
